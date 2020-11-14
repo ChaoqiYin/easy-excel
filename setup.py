e2fd4abc4df5905537b3ffa5629e2c97af9144fe
ChaoqiYin <305111632@qq.com>
2020-11-14 13:10:21 +0800

    setup更新

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/ChaoqiYin/easy-excel.git'
 EMAIL = '305111632@qq.com'
 AUTHOR = 'ChaoqiYin'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.0.2'
+VERSION = '2.0.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
